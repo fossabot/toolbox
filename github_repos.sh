c359fe0856d7a3a2dc0c101b40b82c6cbf0ffed6
daemonna <32168685+peterducai@users.noreply.github.com>
2018-06-25 14:49:55 +0200

    Create github_repos.sh

---
+++ github_repos.sh
@@ -0,0 +1,13 @@
+
+
+#LANGUAGES
+https://github.com/nodejs/docker-node.git
+https://github.com/dotnet/dotnet-docker.git
+https://github.com/docker-library/ruby.git
+https://github.com/perl6/docker.git
+https://github.com/docker-library/python.git
+https://github.com/docker-library/openjdk.git
+https://github.com/docker-library/php.git
+https://github.com/docker-library/golang.git
+
+#SERVERS
