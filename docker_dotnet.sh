eb2fe16fe2f586a7700151bb9ec8dc1ce3b50aa0
daemonna <32168685+peterducai@users.noreply.github.com>
2018-06-25 13:45:26 +0200

    Create docker_dotnet.sh

---
+++ docker_dotnet.sh
@@ -0,0 +1,4 @@
+docker pull registry.access.redhat.com/dotnet/dotnet-21-rhel7
+docker pull registry.access.redhat.com/dotnet/dotnet-20-rhel7
+ddocker pull registry.access.redhat.com/dotnet/dotnetcore-11-rhel7
+ddocker pull registry.access.redhat.com/dotnet/dotnetcore-10-rhel7
