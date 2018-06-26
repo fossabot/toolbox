a094d4542aa34a451d7ce88911d9b12632ab5525
daemonna <32168685+peterducai@users.noreply.github.com>
2018-06-26 10:24:25 +0200

    Update github_repos.sh

--- github_repos.sh
+++ github_repos.sh
@@ -1,4 +1,6 @@
 
+# DOCKER
+https://github.com/docker-library/official-images.git
 
 #LANGUAGES
 https://github.com/nodejs/docker-node.git
@@ -9,5 +11,8 @@ https://github.com/docker-library/python.git
 https://github.com/docker-library/openjdk.git
 https://github.com/docker-library/php.git
 https://github.com/docker-library/golang.git
+https://github.com/docker-library/golang.git
+https://github.com/docker-library/julia.git
+https://github.com/docker-library/gcc.git
 
 #SERVERS
